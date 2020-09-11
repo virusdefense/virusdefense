036dc4ee91431a0f2f887b0e06e4535db1666796
norangebit <git@norangeb.it>
2020-09-11 20:33:17 +0200

    Use text resource for level board

--- Assets/Scripts/Board/BoardBuilder.cs
+++ Assets/Scripts/Board/BoardBuilder.cs
@@ -16,7 +16,7 @@ public class BoardBuilder : MonoBehaviour
     [SerializeField] private GameObject defenseBlock;
     [SerializeField] private GameObject buildBlock;
     [SerializeField] private GameObject notPlayableBlock;
-    [SerializeField] private string levelFilePath;
+    [SerializeField] private int level;
 
     private const int BlockSide = 2;
 
@@ -25,7 +25,7 @@ public class BoardBuilder : MonoBehaviour
 
     private void Start()
     {
-        var board = ReadBoard(levelFilePath);
+        var board = ReadBoard(level);
         var rowNumber = board.Count;
         var columnNumber = board[0].Count;
         var spawnNumber = 0;
@@ -121,19 +121,13 @@ public class BoardBuilder : MonoBehaviour
         }
     }
 
-    private static List<List<char>> ReadBoard(string filePath)
+    private static List<List<char>> ReadBoard(int level)
     {
-        var reader = new StreamReader(filePath);
-        var lines = reader.ReadToEnd().Split(
-            new[] {"\r\n", "\r", "\n"},
-            StringSplitOptions.None
-        );
-        reader.Close();
-
-        return lines
+        return Resources.Load<TextAsset>(string.Format(LevelBoardFile, level.ToString("00")))
+            .text.Split('\n')
             .Select(line => line.Split(' ')
-                .Select(s => s[0]).ToList())
-            .ToList();
+                .Select(s => s[0]).ToList()
+            ).ToList();
     }
 
     private GameObject GetBlock(char type)
@@ -148,4 +142,6 @@ public class BoardBuilder : MonoBehaviour
 
         return notPlayableBlock;
     }
+
+    private const string LevelBoardFile = "Plain/Board/level_{0}";
 }
