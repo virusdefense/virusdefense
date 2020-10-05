229fd67ab556db40494be25df57b00aaaef0b7e0
norangebit <git@norangeb.it>
2020-10-05 13:11:07 +0200

    Add new pref keys
    - Add level completed
    - Add level unlocked
    - Add level score

--- Assets/Scripts/Utils/Settings/Key.cs
+++ Assets/Scripts/Utils/Settings/Key.cs
@@ -5,5 +5,8 @@ namespace Utils.Settings
         public const string GROUND_TOWER_LEVEL = "GROUND_TOWER_LEVEL";
         public const string LIGHT_TOWER_LEVEL = "LIGHT_TOWER_LEVEL";
         public const string HEAVY_TOWER_LEVEL = "HEAVY_TOWER_LEVEL";
+        public const string GAME_LEVEL_COMPLETED = "GAME_LEVEL_{0}_COMPLETED";
+        public const string GAME_LEVEL_UNLOCKED = "GAME_LEVEL_{0}_UNLOCKED";
+        public const string GAME_LEVEL_STAR = "GAME_LEVEL_{0}_STAR";
     }
 }
